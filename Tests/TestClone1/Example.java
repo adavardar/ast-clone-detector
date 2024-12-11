14039c7a58c70e7116e107858159a3988f72fc95
Ada <97483982+adavardar@users.noreply.github.com>
2024-12-11 12:52:03 +0100

    waaahh

---
+++ Tests/TestClone1/Example.java
@@ -0,0 +1,10 @@
+public class Example {
+    public void f ()
+    {
+        int x=0;
+        int a=1;
+        int b=2;
+        int w=4;
+    }
+
+}
---
+++ Tests/TestClone1/Example2.java
@@ -0,0 +1,9 @@
+public class Example2 {
+    public void d ()
+    {
+        int x=0;
+        int a=1;
+        int b=2;
+        int w=4;
+    }
+}
